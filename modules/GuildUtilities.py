c6c3b849df66123c0bf1724546bfb1c65c761ffc
Joshua Razon <joshrazon132@gmail.com>
2020-05-25 17:53:18 +0800

    Added GuildUtilities module

---
+++ modules/GuildUtilities.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands,tasks
+from discord.utils import get
+
+import sqlite3
+
+import BotConf
+
+
+class GuildUtilities(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(aliases=["rcself"])
+    @commands.has_role(BotConf.name_role_member)
+    async def recordsself(self, ctx):
+        connection = sqlite3.connect("modules/data/guild.db")
+        c = connection.cursor()
+        c.execute(f'''SELECT Total FROM guild WHERE Username = '{ctx.author.display_name}'
+                       ''')
+        total = c.fetchone()
+        c.close()
+        await ctx.channel.send(f"{ctx.author.display_name} has checked-in {total[0]} time(s) this week!")
+        await ctx.message.delete()
+
+    @commands.command(aliases=["rc"])
+    @commands.has_role(BotConf.name_role_member)
+    async def records(self, ctx, *, member: discord.Member):
+        for role in member.roles:
+            if role.name == BotConf.name_role_member:
+                connection = sqlite3.connect("modules/data/guild.db")
+                c = connection.cursor()
+                c.execute(f'''SELECT Total FROM guild WHERE Username = '{member.display_name}'
+                           ''')
+                total = c.fetchone()
+                c.close()
+                await ctx.channel.send(f"{member.display_name} has checked-in {total[0]} time(s) this week!")
+                await ctx.message.delete()
+
+
+def setup(client):
+    client.add_cog(GuildUtilities(client))
